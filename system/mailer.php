92404c35e0b06c4b86e115af2a0d6ba1a15b7b51
Armagan Tekdoner <grifare@users.noreply.github.com>
2018-06-25 13:26:09 -0400

    Delete mailer.php

--- system/mailer.php
+++
@@ -1,9 +0,0 @@
-﻿<?php
-$armagan_mail = "a@grifare.info";
-$subject = "Missile Alert";
-//to send an html-formatted email these headers are needed, but they can be spoofed as well
-$header .= "From:missile@grifare-intelligence.com\n";
-$header .= "MIME-Version:1.0\n";
-$header .= "Content-type: text/html; charset=us-ascii\n";
-// the PHP function that does the mailing job
-mail($armagan_mail,$subject,$message,$header);
